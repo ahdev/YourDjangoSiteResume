4a8f858f78c226a58dc04c4801db2ae226d4b6b6
ahdev <ah_dev@outlook.com>
2015-10-01 00:04:18 -0700

    Update attendee info displayed

--- app/views.py
+++ app/views.py
@@ -8,8 +8,8 @@ from django.template import RequestContext
 from datetime import datetime
 
 YOUR_INFO = {
-    'name' : 'Your name',
-    'bio' : 'What\'s your deal? What do you do?',
+    'name' : 'Super Dev',
+    'bio' : 'Protects the world against spaghetti code and combats elusive bugs.',
     'email' : '', # Leave blank if you'd prefer not to share your email with other conference attendees
     'twitter_username' : 'tweettweet', # No @ symbol, just the handle.
     'github_username' : "fetchpush", 
